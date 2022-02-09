db52c389a2533d8e07947b5532600ac3b38d1a8b
Dheeraj Singh <dheeraj26singh@gmail.com>
2022-02-09 18:47:36 +0530

    Change DB Collection Name

--- uma/src/mcoc_roster.py
+++ uma/src/mcoc_roster.py
@@ -14,7 +14,7 @@ class Roster():
     def get_roster(self, gamename):
         cluster_code = os.environ.get('cluster')
         cluster = MongoClient(cluster_code)
-        db = cluster["MCOC"]["Roster"]  
+        db = cluster["MCOC"]["Account"]  
         x = db.find_one({"game_name":gamename}, {'_id': 0})   
         if x is not None:
             self.roster_dict = x     
