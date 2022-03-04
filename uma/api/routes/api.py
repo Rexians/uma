c69456821b16b1a39619a62c9746720eb7b998f2
Dheeraj Singh <dheeraj26singh@gmail.com>
2022-03-04 15:19:00 +0530

    Rename Folder

--- uma/api/routes/api.py
+++ uma/api/routes/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from ..endpoints import champ_info, roster
+from ..routes import champ_info, roster
 
 # router is to add all routes into 1 and import this into the application
 # To add more, import the specific router from endpoints, then include it in this main router
